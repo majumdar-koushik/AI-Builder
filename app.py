dce95600b8081c26fa72b7eff1d9b20389eace29
Koushik Majumder <majumdar.koushik@gmai.com>
2026-06-29 04:11:06 +0530

    Clean commit

--- app.py
+++ app.py
@@ -1,33 +1,35 @@
 
-from flask import Flask, request
+from flask import Flask, request, render_template
+from flask_sqlalchemy import SQLAlchemy
 from google import genai
 from dotenv import load_dotenv
+
 import os
 
 load_dotenv()
 
-
 app = Flask(__name__)
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///chat.db"
+db = SQLAlchemy(app)
+class ChatHistory(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    question = db.Column(db.String(500))
+    answer = db.Column(db.String(2000))
+with app.app_context():
+    db.create_all()
+
+print("KEY USED =", os.getenv("GEMINI_KEY")[:16])
+print("MODEL =", "gemini-2.0-flash")
 
 client = genai.Client(
     api_key=os.getenv("GEMINI_KEY")
 )
 
+
 @app.route("/")
 def home():
-
-    return """
-    <h2>My AI Assistant</h2>
-
-    <form action="/ask">
-
-        <input name="q" style="width:400px">
-
-        <button type="submit">Ask</button>
-
-    </form>
-    """
-
+    chats = ChatHistory.query.all()
+    return render_template("index.html", answer="", chats=chats)
 
 @app.route("/ask")
 def ask():
@@ -35,16 +37,38 @@ def ask():
     question = request.args.get("q")
 
     try:
-        response = client.models.generate_content(
-        model="gemini-2.5-flash",
-        contents=question
-    )
-        return response.text
+# """ """      """    response = client.models.generate_content(
+#             model="gemini-2.0-flash",
+#             contents=question
+#         ) """ """ """
+        class Fake:
+            text = "TEST ANSWER"
+
+        response = Fake()
+        chat = ChatHistory(
+            question=question,
+            answer=response.text
+        )
+
+        db.session.add(chat)
+        db.session.commit()
+
+
+        return render_template(
+            "index.html",
+            answer=response.text
+        )
 
     except Exception as e:
-        return "Error: " + str(e)
 
+        return render_template(
+            "index.html",
+            answer="Error: " + str(e)
+        )
+with app.app_context():
+    chats = ChatHistory.query.all()
+    print("TOTAL RECORDS =", len(chats))
 
-
-
-app.run()
+#app.run()
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
