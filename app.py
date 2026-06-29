1b2ad711db8c86eb1a7ee208a86ff3ff18aee8a2
Koushik Majumder <majumdar.koushik@gmai.com>
2026-06-29 06:49:17 +0530

    Port changed

--- app.py
+++ app.py
@@ -70,5 +70,7 @@ with app.app_context():
     print("TOTAL RECORDS =", len(chats))
 
 #app.run()
+
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
